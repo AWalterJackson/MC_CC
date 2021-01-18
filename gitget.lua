e91947917b7f3b59c1aebe6d750db1aeeca28899
Alexander Jackson <nantangitan@gmail.com>
2021-01-19 00:02:32 +1100

    File no longer intentionally empty

--- gitget.lua
+++ gitget.lua
@@ -1,1 +1,17 @@
-{File intentionally empty}
+local args = { ... }
+
+local remoteFileName
+
+if string.find(args[1], ".lua") then
+    remoteFileName = args[1]
+else
+    remoteFileName = args[1] .. ".lua"
+end
+
+print("Downloading " .. remoteFileName)
+
+local remoteContent = http.get("https://raw.github.com/AWalterJackson/MC_CC/master/" .. remoteFileName)
+local localFile = fs.open(remoteFileName, "w")
+localfile.write(remoteContent.readAll())
+localfile.close()
+print("Completed")
